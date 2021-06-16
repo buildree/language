2711c8886f6e71d4c3892dce7da8492b24b4e3ba
sitelabhayashi <hayashi@site-lab.jp>
2021-06-16 12:51:15 +0900

    簡略化

--- python.sh
+++ python.sh
@@ -48,73 +48,19 @@ if [ -e /etc/redhat-release ]; then
         start_message
         echo "yum updateを実行します"
         echo ""
-        yum -y update
+        wget wget https://www.logw.jp/download/shell/common/system/update.sh
+        source ./update.sh
         end_message
 
-        #pyenvの設定
-        start_message
-        echo "gitでpyenvをクーロンします"
-        git clone git://github.com/yyuu/pyenv.git /usr/local/pyenv
-        git clone git://github.com/yyuu/pyenv-virtualenv.git /usr/local/pyenv/plugins/pyenv-virtualenv
-        end_message
-
-        #pyenvのインストール
-        start_message
-        echo "起動時に読み込まれるようにします"
-        cat >/etc/profile.d/pyenv.sh <<'EOF'
-export PYENV_ROOT="/usr/local/pyenv"
-export PATH="${PYENV_ROOT}/bin:${PATH}"
-eval "$(pyenv init --path)"
-EOF
-
-        source /etc/profile.d/pyenv.sh
-        end_message
-
-        #pythonの確認
-        start_message
-        echo "pythonのリスト確認"
-        pyenv install --list
-        echo "python3.9.5のインストール"
-        env PYTHON_CONFIGURE_OPTS="--enable-shared" pyenv install 3.9.5
-        echo "pythonの設定を変更"
-        pyenv global 3.9.5
-        end_message
-
-        #pythonの確認
-        start_message
-        echo "pythonの位置を確認"
-        which python
-        echo "pythonのバージョン確認"
-        python --version
-        end_message
-
-        #pipのアップグレード
-        start_message
-        echo "pipのアップグレード"
-        pip install --upgrade pip
-        end_message
+        echo "pythonのインストールをします"
+        wget wget https://www.logw.jp/download/shell/common/system/pyenv.sh
+        source ./pyenv.sh
 
 
         #ユーザー作成
-        start_message
-        echo "centosユーザーを作成します"
-        USERNAME='centos'
-        PASSWORD=$(more /dev/urandom  | tr -d -c '[:alnum:]' | fold -w 10 | head -1)
-
-        useradd -m -G nobody -s /bin/bash "${USERNAME}"
-        echo "${PASSWORD}" | passwd --stdin "${USERNAME}"
-        echo "パスワードは"${PASSWORD}"です。"
-        end_message
-
-        umask 0002
-
-        #ファイルの保存
-        start_message
-        echo "パスワードなどを保存"
-        cat <<EOF >/root/pass.txt
-ログインユーザー
-centos = ${PASSWORD}
-EOF
+        echo "pythonのインストールをします"
+        wget wget https://www.logw.jp/download/shell/common/user/centosonly.sh
+        source ./centosonly.sh
 
         #コピー作成
         cp /root/pass.txt /home/centos/
